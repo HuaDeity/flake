fe5bcb1a21d21cd55d7ec6192c14eeb95c84c780
HuaDeity <hello@huadeity.com>
2025-10-12 16:42:22 +0800

    Add containerd systemd

--- nix/hosts/Lab103/system-configuration.nix
+++ nix/hosts/Lab103/system-configuration.nix
@@ -62,6 +62,38 @@ in
 
         wantedBy = [ "multi-user.target" ];
       };
+
+      containerd = {
+        enable = true;
+
+        unitConfig = {
+          Description = "containerd container runtime";
+          Documentation = [ "https://containerd.io" ];
+          After = [
+            "network.target"
+            "dbus.service"
+          ];
+        };
+
+        serviceConfig = {
+          ExecStartPre = "-/sbin/modprobe overlay";
+          ExecStart = "${pathDir}/bin/containerd";
+
+          Type = "notify";
+          Delegate = "yes";
+          KillMode = "process";
+          Restart = "always";
+          RestartSec = 5;
+
+          LimitNPROC = "infinity";
+          LimitCORE = "infinity";
+
+          TasksMax = "infinity";
+          OOMScoreAdjust = -999;
+        };
+
+        wantedBy = [ "multi-user.target" ];
+      };
     };
   };
 }
