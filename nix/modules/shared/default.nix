dca2b96d83e0fe313f18142cf0f1650e43f7f154
HuaDeity <hello@huadeity.com>
2025-10-17 16:01:51 +0800

    Fix bug

--- nix/modules/shared/default.nix
+++ nix/modules/shared/default.nix
@@ -12,17 +12,12 @@
 
   config =
     let
-      # Check if we're in a home-manager context (which has submoduleSupport option)
-      # If not in home-manager, we should apply the system-level nix settings
       isHomeManager = lib.hasAttrByPath [ "submoduleSupport" "enable" ] options;
+      hasGc = lib.hasAttrByPath [ "nix" "gc" ] options;
     in
     lib.optionalAttrs (!isHomeManager) {
       nix.package = pkgs.nix;
-
-      # This part is new. It only adds the 'nix.gc' attribute set if the
-      # option has been declared somewhere in the module system.
-      nix.gc = lib.mkIf (lib.hasAttrByPath [ "nix" "gc" ] options) {
-        automatic = true;
-      };
+    } // lib.optionalAttrs (!isHomeManager && hasGc) {
+      nix.gc.automatic = true;
     };
 }
