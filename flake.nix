21448703d994ff3351f0afad7a8924abf6252e51
HuaDeity <hello@huadeity.com>
2025-11-16 20:03:33 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -8,8 +8,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flox-nixpkgs.url = "github:flox/nixpkgs/stable";
 
     flox.url = "github:flox/flox/latest";
+    flox.inputs.nixpkgs.follows = "flox-nixpkgs";
+
+    flox-manifest-fetch.url = "github:HuaDeity/flox-manifest-fetch";
+    flox-manifest-fetch.inputs.flox.follows = "flox";
+    flox-manifest-fetch.inputs.nixpkgs.follows = "flox-nixpkgs";
+
     devshell.url = "github:numtide/devshell";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
@@ -25,7 +32,6 @@
     index-database.inputs.nixpkgs.follows = "nixpkgs";
 
     pkgflow.url = "github:HuaDeity/pkgflow";
-    flox-manifest-fetch.url = "github:HuaDeity/flox-manifest-fetch";
   };
 
   outputs =
